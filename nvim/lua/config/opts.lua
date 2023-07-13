55131ce7c4708db00d0631ea097a4e551adbb047
HelloThere57 <ajs.mail.home@gmail.com>
2023-07-13 10:22:03 -0400

    nvim and rc files

---
+++ nvim/lua/config/opts.lua
@@ -0,0 +1,10 @@
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+local tabsize = 4
+vim.opt.tabstop = tabsize
+vim.opt.shiftwidth = tabsize
+vim.opt.expandtab = false
+
+vim.opt.wrap = false
